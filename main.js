785e7cca113c94a4b2e5bdfe742e0e7aedef072d
thinkdevcode <thinkdevcode+pub@gmail.com>
2016-08-26 01:17:14 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false, transparent: true})
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.on('closed', () => mainWindow = null)
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => app.quit())
+app.on('activate', () => {
+  if (mainWindow === null)
+    createWindow()
+})
